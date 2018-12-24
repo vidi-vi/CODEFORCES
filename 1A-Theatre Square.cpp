ac89ce8c0629b92bd907df84b4ab6ce66ab4f7e4
Vidisha Sethi <43785947+vidi-vi@users.noreply.github.com>
2018-12-24 16:17:15 +0530

    Create 1A-Theatre Square.cpp

---
+++ 1A-Theatre Square.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{   double n,m,a;
+    cin>>n;cin>>m;cin>>a;
+    long long r;
+    r= ceil(n/a)*ceil(m/a);
+    cout<<r;
+
+}
